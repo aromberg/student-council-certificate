09246f6bc001b1bb01f6c25787d4d9a4bb03188f
Adrian Romberg <adrian.romberg@icloud.com>
2023-01-02 18:38:46 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const { jsPDF } = window.jspdf;
+
+function downloadPDF() {
+    const doc = new jsPDF({
+                           orientation: 'p',
+                           unit: 'mm',
+                           format: 'a4',
+                           putOnlyUsedFonts:true
+                          });
+    doc.setFont('Helvetica');
+    moment.locale("de-DE");
+    var docWidth = doc.internal.pageSize.getWidth();
+
+    // Form inputs
+    fname = document.getElementById('fname').value;
+    lname = document.getElementById('lname').value;
+    bday = document.getElementById('bday').value;
+    smonth = document.getElementById('smonth').value;
+    emonth = document.getElementById('emonth').value;
+
+    // Logo
+    var img = new Image();
+    img.src = 'img/bioinf_fs_logo.png';
+    doc.addImage(img, "PNG", docWidth-45, 10, 35, 35);
+
+    // Adress field
+    doc.setFontSize(6);
+    doc.text("Fachschaft Bioinformatik · Robert-Mayer-Straße 11-15 · 60325 Frankfurt am Main", 25, 39.7, {maxWidth: 85});
+
+    doc.setFontSize(11);
+    doc.text([fname + " " + lname,
+              "Willemerstr. 29",
+              "60594 Frankfurt"],
+             25, 44.7, {maxWidth: 85});
+
+    // Info block
+    doc.setFontSize(11);
+    doc.text(moment().format("L"),
+             docWidth-40, 72);
+
+
+    // Text
+    doc.setFontSize(11);
+    doc.text(["Sehr geehrte Damen und Herren",
+              "",
+              "hiermit bestätigen wir, dass " + fname + " " + lname + ", geb. am " + moment(bday).format("L") + ", sich von " + moment(smonth).format("MMMM YYYY") + " bis " + moment(emonth).format("MMMM YYYY") + " ehrenamtlich in der Fachschaft des Studiengangs Bioinformatik an der Goethe-Universität Frankfurt am Main engagiert hat.",
+              "",
+              "Zielsetzung der Fachschaft Bioinformatik ist es, die Vernetzung sowohl unter Studierenden als auch zwischen Studierenden und Dozierenden zu fördern und den Studiengang kontinuierlich inhaltlich sowie auch strukturell zu verbessern."],
+            25, 87, {maxWidth: docWidth-45});
+    
+    doc.save("bescheinigung_fachschaft_bioinformatik.pdf");
+
+
+
+
+}
