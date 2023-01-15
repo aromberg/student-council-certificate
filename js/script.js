f8ab5927ea14aba6b9863f452f1abfc8569ecdbe
Adrian Romberg <adrian.romberg@icloud.com>
2023-01-15 09:35:16 +0100

    add more individualization based on form inputs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const { jsPDF } = window.jspdf;
 
+document.getElementById('emonth').value = moment().format("YYYY-MM-DD");
+
+document.getElementById('active').onchange = function() {
+  document.getElementById('emonth').disabled = this.checked;
+  document.getElementById('emonth').value = moment().format("YYYY-MM-DD");
+};
+
 function downloadPDF() {
     const doc = new jsPDF({
                            orientation: 'p',
@@ -12,13 +19,30 @@ function downloadPDF() {
     var docWidth = doc.internal.pageSize.getWidth();
 
     // Form inputs
+    pronoun = document.getElementById('pronoun').value;
     fname = document.getElementById('fname').value;
     lname = document.getElementById('lname').value;
     street = document.getElementById('street').value;
     zipCity = document.getElementById('zipCity').value;
     bday = document.getElementById('bday').value;
     smonth = document.getElementById('smonth').value;
+    active = document.getElementById('active').checked
     emonth = document.getElementById('emonth').value;
+    customText = document.getElementById('customText').value;
+    signature = document.getElementById('signature').value;
+
+    // Input-based phrases
+    introduction = `hiermit bestätigen wir, dass ${fname} ${lname}, geb. am ${moment(bday).format("L")}, sich `
+    conclusion = `Wir danken ${fname} für ${pronoun=="sie" ? 'ihr' : 'sein'} Engagement und `
+    if (active) {
+        introduction += `seit ${moment(smonth).format("MMMM YYYY")} ehrenamtlich in der Fachschaft des Studiengangs Bioinformatik an der Goethe-Universität Frankfurt am Main engagiert.`;
+        conclusion += `freuen uns auf die weitere Zusammenarbeit!`;
+    }
+    else {
+        introduction += `von ${moment(smonth).format("MMMM YYYY")} bis ${moment(emonth).format("MMMM YYYY")} ehrenamtlich in der Fachschaft des Studiengangs Bioinformatik an der Goethe-Universität Frankfurt am Main engagiert hat.`;
+        conclusion += `wünschen ${pronoun=="sie" ? 'ihr' : 'ihm'} für ${pronoun=="sie" ? 'ihre' : 'seine'} Zukunft auch weiterhin alles Gute!`;
+    }
+
 
     // Logo
     var img = new Image();
@@ -48,29 +72,23 @@ function downloadPDF() {
     // Title
     doc.setFontSize(11);
     doc.setFont('Helvetica', 'bold');
-    doc.text("Bescheinigung über ehrenamtliches Engagement", 25, 91.46); // line height: 4.46
-    //console.log(doc.getTextDimensions("Bescheinigung über ehrenamtliches Engagement"))
+    doc.text("Bescheinigung über ein ehrenamtliches Engagement", 25, 100);
 
     // Text
     doc.setFont('Helvetica', 'normal');
     doc.text(["Sehr geehrte Damen und Herren,",
               "",
-              "hiermit bestätigen wir, dass " + fname + " " + lname + ", geb. am " + moment(bday).format("L") + ", sich von " + moment(smonth).format("MMMM YYYY") + " bis " + moment(emonth).format("MMMM YYYY") + " ehrenamtlich in der Fachschaft des Studiengangs Bioinformatik an der Goethe-Universität Frankfurt am Main engagiert hat.",
+              introduction,
               "",
-              "Zielsetzung der Fachschaft Bioinformatik ist es, die Vernetzung sowohl unter Studierenden als auch zwischen Studierenden und Dozierenden zu fördern und den Studiengang kontinuierlich inhaltlich sowie auch strukturell zu verbessern.",
+              "Die Fachschaft Bioinformatik ist eine Interessensgemeinschaft und Organisation der studentischen Selbstverwaltung, die sich speziell an Studierende der Bioinformatik richtet. Zielsetzung der Fachschaft Bioinformatik ist es, die Vernetzung sowohl unter Studierenden als auch zwischen Studierenden und Dozierenden zu fördern und den Studiengang kontinuierlich inhaltlich sowie auch strukturell zu verbessern. Hierzu organisieren studentische Mitglieder Veranstaltungen wie einen Orientierungstag für neue Studierende, Stammtische sowie weitere Events. Die Fachschaft stellt außerdem einen Discord-Server zur studiengangsinternen Kommunikation bereit und dient als erster Ansprechpartner für Studierende. Hochschulpolitisch engagieren sich Mitglieder in universitären Gremien. Die Fachschaft geht weiterhin aktiv in den Austausch mit Dozierenden, um die Interessen der Studierenden zu vertreten.",
               "",
-              fname + " " + lname + " leistete einen wertvollen Beitrag zur Fachschaftsarbeit, indem er sich regelmäßig an Planungen zu Netzwerktreffen für Studierende beteiligte und Einführungsveranstaltungen der Fachschaft für Studienanfänger*innen mitorgansierte.",
+              customText,
               "",
-              "Wir danken " + fname + " " + lname + " für sein Engagement und wünschen ihm für seine Zukunft weiterhin alles Gute.",
+              conclusion,
               "",
               "Mit freundlichen Grüßen",
-              "",
-              "Fachschaft Bioinformatik"],
-            25, 100.38, {maxWidth: docWidth-45});
+              signature],
+            25, 108.92, {maxWidth: docWidth-45});
     
     doc.save("bescheinigung_fachschaft_bioinformatik.pdf");
-
-
-
-
 }
